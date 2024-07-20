f40772cadbd5e99bd1b543bf31b42d0f61b3b933
AliyaBiggsNavos <aliya.biggs@navos.co.uk>
2024-07-20 10:29:58 +0100

    refactor: :lipstick: UI styling now correct for mobile view

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 import SideNavBar from "./components/sidenavbar";
 
 import Discover from "./pages/discover";
 
-import './css/app.css'; 
+import "./css/app.css";
 
 export default function App(props: any) {
   return (
@@ -15,7 +15,7 @@ export default function App(props: any) {
         <SideNavBar {...props} />
         <ContentWrapper>
           <Switch>
-            <Route path="/discover" component={Discover} {...props}/>
+            <Route path="/discover" component={Discover} {...props} />
           </Switch>
         </ContentWrapper>
       </PageContainer>
@@ -23,11 +23,25 @@ export default function App(props: any) {
   );
 }
 
+const breakpoints = {
+  mobile: "480px",
+  desktop: "1024px",
+};
+
+const media = {
+  mobile: `(max-width: ${breakpoints.mobile})`,
+  desktop: `(max-width: ${breakpoints.desktop})`,
+};
 
 const ContentWrapper = styled.main`
   padding-left: 280px;
-`
+
+  @media ${media.mobile} {
+    padding-left: 10px;
+    padding-right: 10px;
+  }
+`;
 
 const PageContainer = styled.main`
   overflow-x: hidden;
-`
+`;
